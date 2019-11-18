c03be5ac4c61e9b3551b79248ecc838331237f79
beyonce knowles <beyonce@thinkful.com>
2019-11-17 21:36:18 -0600

    adding

---
+++ index.js
@@ -0,0 +1,53 @@
+function myShoppingList() {
+  $('#js-shopping-list-form').submit(function(e){
+    event.preventDefault();
+
+    const food = $('.js-shopping-list-entry').val();
+
+
+    
+      $('.shopping-list').append(`<li>
+        <span class="shopping-item">${food}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      <li>`);
+      ;
+  
+});
+
+$('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
+  $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+
+
+$('.shopping-list').on('click', '.shopping-item-delete', function(e) {
+  $(this).closest('li').remove();
+});
+}
+ 
+
+
+
+$(myShoppingList);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
